74e6810d4119b11f7518aadac2ad3a180625454f
colmjude <colmjude@gmail.com>
2024-07-31 14:35:24 +0300

    remove schema tab

--- application/blueprints/main/views.py
+++ application/blueprints/main/views.py
@@ -27,7 +27,6 @@ main = Blueprint("main", __name__)
 def get_tab_list(dataset):
     return [
         {"title": "Records", "url": url_for("main.dataset", id=dataset.dataset)},
-        {"title": "Schema", "url": url_for("main.schema", id=dataset.dataset)},
         {"title": "Links", "url": url_for("main.links", id=dataset.dataset)},
         {"title": "History", "url": url_for("main.history", id=dataset.dataset)},
         {"title": "Changes", "url": url_for("main.change_log", id=dataset.dataset)},
